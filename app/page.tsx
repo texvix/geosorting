b3738dfcfc5ff791e8d086953745d136dfa08fa2
texvix <texvixx@gmail.com>
2025-07-01 12:32:37 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 import * as XLSX from "xlsx";
 import dynamic from "next/dynamic";
 
